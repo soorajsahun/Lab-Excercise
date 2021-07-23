a57e37eff5006822019ad4c0e7f68efcbede144c
SurajSahuGitHub <79203537+soorajsahun@users.noreply.github.com>
2021-07-23 16:06:12 +0530

    Update FindLCM.java

--- FindLCM.java
+++ FindLCM.java
@@ -1,4 +1,4 @@
-package hit.day5;
+package hit.day5_Lab1;
 
 import java.util.Scanner;
 
@@ -9,13 +9,16 @@ public class FindLCM {
 		int x=scan.nextInt();
 		int y=scan.nextInt();
 		int lcm;
+//		1st method)
+//		lcm=x>y?x:y;
+		
+//		2nd method)
 		for(lcm=x>y?x:y;lcm<=(x*y);lcm=lcm+(x>y?x:y))
 		{
 			if(lcm%x==0 && lcm%y==0)
 			break;
 		}
 		System.out.printf("Lcm of %d and %d is %d",x,y,lcm);
+		scan.close();
 }
 }
-
-		
